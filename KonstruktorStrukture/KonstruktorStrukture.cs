9bcbc02337efde448ca82eace7ca65b18f6edb25
Lucija Ivanković VSITE2015 <luivanko@vsite.hr>
2015-11-12 21:14:25 +0100

    KonstruktorStrukture Done

--- KonstruktorStrukture/KonstruktorStrukture.cs
+++ KonstruktorStrukture/KonstruktorStrukture.cs
@@ -6,18 +6,33 @@ namespace Vsite.CSharp
     {
         struct MojaStruktura
         {
+            //public MojaStruktura() 
+            //{
+            //    a=10;
+            //    s="pero";  
+            //}
+            public MojaStruktura(int a, string s) 
+            {
+                this.a=a;
+                this.s=s;
+            }
+
             public int a;
             public string s;
         }
 
         static void Main(string[] args)
         {
-            // TODO: Stvoriti objekt strukture MojaStruktura i ispisati vrijednosti članova
+            // Stvoriti objekt strukture MojaStruktura i ispisati vrijednosti članova
+            MojaStruktura mk = new MojaStruktura();
+            Console.WriteLine(mk.a);
+            Console.WriteLine(mk.s);
 
 
-
-            // TODO: U strukturi MojaStruktura promijeniti prazan konstruktor tako da prima dva argumenta kojima se inicijaliziraju članovi te ponoviti gornji postupak
-
+            // U strukturi MojaStruktura promijeniti prazan konstruktor tako da prima dva argumenta kojima se inicijaliziraju članovi te ponoviti gornji postupak
+            MojaStruktura mk1 = new MojaStruktura(3,"zagreb");
+            Console.WriteLine(mk1.a);
+            Console.WriteLine(mk1.s);
 
             Console.ReadKey();
 
