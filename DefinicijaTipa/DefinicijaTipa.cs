5d957c68045f2f3161bff1aaf25e0317583c72e2
Lucija Ivanković VSITE2015 <luivanko@vsite.hr>
2015-11-12 20:09:33 +0100

    DefinicijaTipa Done

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -4,21 +4,27 @@ namespace Vsite.CSharp
 {
     class MojaKlasa
     {
-        // TODO: Dodati jedna privatno polje (podatkovni član)
+        // Dodati jedna privatno polje (podatkovni član)
+        private int x = 1;
 
-
-        // TODO: Dodati javnu metodu koja će ispisati podatkovni član
-
+        // Dodati javnu metodu koja će ispisati podatkovni član
+        public int IspisiX()
+        {
+            return this.x;
+        }
     }
 
 
     struct MojaStruktura
     {
-        // TODO: Dodati jedna privatno polje (podatkovni član)
+        // Dodati jedna privatno polje (podatkovni član)
+        string s = "tekst";
 
-
-        // TODO: Dodati javnu metodu koja će ispisati podatkovni član
-
+        // Dodati javnu metodu koja će ispisati podatkovni član
+        public string DajTekst() 
+        {
+            return s;
+        }
     }
 
 
@@ -27,7 +33,7 @@ namespace Vsite.CSharp
         static void Main(string[] args)
         {
             // TODO: Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu
-
+           
             // TODO: Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu
 
             Console.ReadKey();
