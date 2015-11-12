0e222dcd915de30b055430e8e581608662680362
Lucija Ivanković VSITE2015 <luivanko@vsite.hr>
2015-11-12 20:49:14 +0100

    KonstruktorKlase Done

--- KonstruktorKlase/KonstruktorKlase.cs
+++ KonstruktorKlase/KonstruktorKlase.cs
@@ -5,6 +5,16 @@ namespace Vsite.CSharp
 {
     class MojaKlasa
     {
+        public MojaKlasa(int a, string s)
+        {
+            this.a = a;
+            this.s = s;
+        }
+        public MojaKlasa() 
+        {
+            a = 10;
+            s = "pero";
+        }
         public int a;
         public string s;
     }
@@ -13,16 +23,22 @@ namespace Vsite.CSharp
     {
         static void Main(string[] args)
         {
-            // TODO: Stvoriti objekt klase MojaKlasa i ispisati vrijednosti članova
+            // Stvoriti objekt klase MojaKlasa i ispisati vrijednosti članova
+            MojaKlasa X = new MojaKlasa();
+            Console.WriteLine(X.a);
+            Console.WriteLine(X.s);
 
 
-
-            // TODO: U klasu MojaKlasa dodati prazan konstruktor koji inicijalizira svoje članove na neku vrijednost te ponoviti gornji postupak
-
+            //  U klasu MojaKlasa dodati prazan konstruktor koji inicijalizira svoje članove na neku vrijednost te ponoviti gornji postupak
+            MojaKlasa X2 = new MojaKlasa();
+            Console.WriteLine(X2.a);
+            Console.WriteLine(X2.s);
 
 
             // TODO: U klasi MojaKlasa promijeniti prazan konstruktor tako da prima dva argumenta kojima se inicijaliziraju članovi te ponoviti gornji postupak
-
+            MojaKlasa X3 = new MojaKlasa(100, "stol");
+            Console.WriteLine(X3.a);
+            Console.WriteLine(X3.s);
 
             Console.ReadKey();
         }
